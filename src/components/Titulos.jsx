9246e1954a2b2c87b8b9b8fb9f9a72454fe0a412
Chachi20 <santanahormigaencarna@gmail.com>
2023-06-30 13:48:54 +0100

    Texto hecho

---
+++ src/components/Titulos.jsx
@@ -0,0 +1,15 @@
+const Titulo = () => {
+
+    return (
+
+        <div>
+            <h1>Organize Your Time And Start Getting Results</h1>
+            <p>Sync is the first mobile app on the market to harness
+                power of social connections to help you stop procrastinating
+                and start getting things done. Give it a try and see the
+                changes right away</p>
+        </div>
+    )
+}
+
+export default Titulo;
