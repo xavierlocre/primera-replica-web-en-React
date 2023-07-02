568c3079fa4ca19232c5cc2b0c43623ea87223a6
Chachi20 <santanahormigaencarna@gmail.com>
2023-07-02 23:48:53 +0100

    añadir imagen e intentar colocar en columnas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import Titulo from './components/Titulos'
 import Estilos from './components/atomos/Estilos.css'
 
-
 function App() {
   
 
--- src/components/Titulos.jsx
+++ src/components/Titulos.jsx
@@ -2,15 +2,27 @@ const Titulo = () => {
 
     return (
 
-        <div className="Texto">
-            <div>
-                <h1>Organize Your Time And Start Getting Results</h1>
-                <p> Sync is the first mobile app on the market to harness 
-                    power of social connections to help you stop procrastinating
-                    and start getting things done. Give it a try and see 
-                        changes right away
-                   </p>
-            </div></div>
+
+
+        <div>
+            <img src="src\screenshot-2.jpg" alt="imagen de la derecha" /> 
+            <h1 className="h1">Organize Your Time And Start Getting Results</h1>
+            <p className="p" > Sync is the first mobile app on the market to harness
+                power of social connections to help you stop procrastinating
+                and start getting things done. Give it a try and see
+                changes right away
+            </p>
+            <ul>
+                <li className="li">Anlyse and evaluate your current status and productivity</li>
+                <li className="li">Begin monitoring your day to day routine with Sync app</li>
+                <li className="li">See the improved results in no more than a couple of weeks</li>
+            </ul>
+
+            <button className="boton">LIGHTBOX</button>
+
+        </div>
+
+
     )
 }
 
