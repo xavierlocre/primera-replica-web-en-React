a45acd3d5e6bb511b0fec82128b965d9185eb25b
Chachi20 <santanahormigaencarna@gmail.com>
2023-07-03 23:59:32 +0100

    El texto que no consigo poner en el contenedor

--- src/components/Texto.jsx
+++ src/components/Texto.jsx
@@ -2,9 +2,10 @@ const Texto= () => {
 
     return (
         
-       <div>
-       <h2 className="h2">Organize Your Time
+       <div className="contendor">
+       <h2>Organize Your Time And Start Getting Results
        </h2>
+
        </div>
    
     
