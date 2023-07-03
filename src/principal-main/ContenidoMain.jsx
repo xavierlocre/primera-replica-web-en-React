18fa7a9e01e49b2b2278cb6d46c7da5208189be3
xavi <xavilok@gmail.com>
2023-07-03 09:38:27 +0200

    cambios para colocar imagen

---
+++ src/principal-main/ContenidoMain.jsx
@@ -0,0 +1,11 @@
+import CirculoAzul from './circulo_azul/CirculoAzul'
+const ContenidoMain = () => {
+
+    return(
+        <div>
+            <CirculoAzul/>
+        </div>
+    )
+}
+
+export default ContenidoMain
---
+++ src/principal-main/circulo_azul/CirculoAzul.jsx
@@ -0,0 +1,12 @@
+
+
+const CirculoAzul = () => {
+
+  return (
+    <div>
+        <img className="movil" src='src\assets\img\decorative-blue-circle.svg' />
+    </div>
+  )
+}
+
+export default CirculoAzul
